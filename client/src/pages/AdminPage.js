b3036442f719671c5368aff64835e094a7c2c6a0
deeismyname <divinesconty@gmail.com>
2024-11-29 15:50:29 +0000

    finishing off the admin for my profile

--- client/src/pages/AdminPage.js
+++ client/src/pages/AdminPage.js
@@ -8,19 +8,8 @@ const AdminPage = () => {
   return (
     <>
       <Header />
-
-      <main className="d-flex flex-column">
-        {/* Messages Section */}
-        <section style={{ height: '100vh', overflow: 'auto' }}>
-          <Messages />
-        </section>
-
-        {/* Add New Project Section */}
-        <section style={{ height: '100vh', backgroundColor: '#f8f9fa' }}>
-          <AddProject />
-        </section>
-      </main>
-
+      <Messages />
+      <AddProject />
       <Footer />
     </>
   );
